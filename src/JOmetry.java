2b41ddcad5e14cbb8a08dc8ea851ef96dd31304b
Dynosaur <heracassia@gmail.com>
2019-09-24 23:41:26 -0500

    Misc changes

--- src/JOmetry.java
+++ src/JOmetry.java
@@ -1,78 +1,19 @@
-import symbol.constant.FloatingPoint;
-import symbol.constant.Integer;
-
+/**
+ * @author  Alejandro Doberenz
+ * @version 1.0
+ * @since   9/12/2019
+ *
+ * Runs the JOmetry program.
+ */
 public class JOmetry {
 
-    static class Operator {
-
-        public static char ADD = '+';
-        public static char SUB = '-';
-        public static char MUL = '*';
-        public static char DIV = '/';
-        public static char EXP = '^';
-
-        public static final char[] OPERATORS = { ADD, SUB, MUL, DIV, EXP };
-
-        public static boolean isOperator(char symbol) {
-            for(char operator : OPERATORS)
-                if(symbol == operator) return true;
-            return false;
-        }
-
-    }
-
-    enum Type {
-        INTEGER,
-        FLOAT,
-        VARIABLE,
-        OPERATOR,
-        EXPRESSION,
-        EQUATION
-    }
-
-    static class ForeignSymbolException extends Exception {
-
-        public ForeignSymbolException(char symbol) {
-            super(symbol + " is not a recognized symbol!");
-        }
-
-    }
-
-    static Type determine(String input) throws ForeignSymbolException {
-        try {
-            java.lang.Integer.parseInt(input);
-            return Type.INTEGER;
-        } catch(NumberFormatException notInt) {
-            try {
-                double d = Double.parseDouble(input);
-                if(d - (int) d == 0) {
-                    return Type.INTEGER;
-                }
-                return Type.FLOAT;
-            } catch(NumberFormatException notFloat) {
-                if(input.length() == 1) {
-                    char symbol = input.charAt(0);
-                    if(Character.isLetter(symbol)) return Type.VARIABLE;
-                    else if(Operator.isOperator(symbol)) return Type.OPERATOR;
-                    else throw new ForeignSymbolException(symbol);
-                } else {
-                    if(input.contains("=")) {
-                        return Type.EQUATION;
-                    } else {
-                        return Type.EXPRESSION;
-                    }
-                }
-            }
-        }
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(Term.isTerm("x5 - 2"));
-        System.out.println(Term.isTerm("x5 + -2"));
-        Token.parse("x5 - 2");
-        FloatingPoint f = new FloatingPoint(2.215);
-        System.out.println(f.toWordString());
+        rsrc.Resources.loadResources();
+        /*
+        SymbolParser parser = new SymbolParser("y = 2.3x + b");
+        parser.toSymbolList();
+         */
+        System.out.println(equation.NumberAggregator.aggregateNumbers("y = 10.25x + 20.1"));
     }
 
 }
--- src/Term.java
+++ src/Term.java
@@ -1,6 +1,8 @@
+import symbol.Symbol;
+
 public class Term {
 
-    private String termString;
+    private String termString, termStrictString;
 
     public Term(String symbol) {
 
@@ -8,6 +10,19 @@ public class Term {
 
     public static boolean isTerm(String test) {
         return !test.contains("+");
+    }
+
+    public Symbol[] toSymbolArray() {
+        StringBuilder string = new StringBuilder();
+        for(int i = 0; i < termString.length(); i++) {
+
+        }
+        return null;
+    }
+
+    public static Term toStrictTerm(Term term) {
+        Symbol[] symbols = term.toSymbolArray();
+        return null;
     }
 
 }
