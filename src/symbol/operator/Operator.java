b5f583e9708dac899d448764814084797a8bb65a
Dynosaur <heracassia@gmail.com>
2019-09-25 23:06:29 -0500

    Added createOperator method

--- src/symbol/operator/Operator.java
+++ src/symbol/operator/Operator.java
@@ -1,11 +1,11 @@
 package symbol.operator;
 
 /**
- * @author  Alejandro Doberenz
- * @version 1.2
- * @since   9/20/2019
- *
  * Represents a mathematical operator, such as adding or subtracting.
+ *
+ * @author  Alejandro Doberenz
+ * @version 1.2.1
+ * @since   9/20/2019
  */
 public class Operator extends symbol.Symbol {
 
@@ -15,7 +15,7 @@ public class Operator extends symbol.Symbol {
     public static Operator DIVIDE = new Operator("/");
     public static Operator EQUALS = new Operator("=");
 
-    public static Operator[] operators = {ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUALS};
+    public final static Operator[] OPERATORS = {ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUALS};
 
     public static boolean isOperator(Object obj) {
         if(obj instanceof Operator) return true;
@@ -23,13 +23,25 @@ public class Operator extends symbol.Symbol {
             String string;
             if(obj instanceof String) string = (String) obj;
             else string = String.valueOf(obj);
-            for(Operator op : operators)
+            for(Operator op : OPERATORS)
                 if(string.equals(op.getSymbol())) return true;
             return false;
         } else return false;
     }
 
-    public Operator(String symbol) {
+    public static Operator createOperator(Object obj) {
+        if(!isOperator(obj)) throw new IllegalArgumentException("Could not create Operator: obj could not be converted to an Operator.");
+        if(obj instanceof String || obj instanceof Character) {
+            String string;
+            if(obj instanceof String) string = (String) obj;
+            else string = String.valueOf(obj);
+            for(Operator op : OPERATORS)
+                if(string.equals(op.getSymbol())) return op;
+            throw new IllegalArgumentException("Could not Create Operator: obj did not match any existing Operators.");
+        } else throw new IllegalArgumentException("Could not create Operator: obj is not a String or Character.");
+    }
+
+    private Operator(String symbol) {
         super(symbol);
     }
 
