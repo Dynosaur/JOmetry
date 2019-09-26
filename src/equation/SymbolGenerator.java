0a3ab9472e76f6b1d3a5b853090226c7d209a343
Dynosaur <heracassia@gmail.com>
2019-09-25 23:06:47 -0500

    Created SymbolGenerator

---
+++ src/equation/SymbolGenerator.java
@@ -0,0 +1,18 @@
+package equation;
+
+import java.util.ArrayList;
+
+import symbol.Symbol;
+
+import static util.Symbols.createSymbol;
+
+public class SymbolGenerator {
+
+    public static ArrayList<Symbol> generateSymbols(ArrayList<String> strings) {
+        ArrayList<Symbol> symbols = new ArrayList<>(0);
+        for(String string : strings)
+            symbols.add(createSymbol(string));
+        return symbols;
+    }
+
+}
