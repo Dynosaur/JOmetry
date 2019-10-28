733aac03487c8a3dd5c346bd4e513e3bdfebcb77
Dynosaur <heracassia@gmail.com>
2019-10-27 21:29:14 -0500

    Reader now prints out results

--- src/main/java/rsrc/ConfigReader.java
+++ src/main/java/rsrc/ConfigReader.java
@@ -9,31 +9,37 @@ public class ConfigReader {
 
     private File configFile;
 
-    private ArrayList<String> fileLines;
+    private ArrayList<String> fileLines = new ArrayList<>();
 
     public ConfigReader(File file) {
-        if(!file.exists()) throw new IllegalArgumentException("File \'" +  file.getPath() + " could not be found.");
+        if(!file.exists())
+            throw new IllegalArgumentException("File \'" +  file.getPath() + " could not be found.");
         configFile = file;
-        fileLines = new ArrayList<>();
     }
 
     void read() {
+        int lineNumber = 0;
         try {
-            Scanner scanner = new Scanner(configFile);
-            while(scanner.hasNext())
-                fileLines.add(scanner.next());
+            Scanner scanner = new Scanner(configFile).useDelimiter("");
+            while(scanner.hasNext()) {
+                String input = scanner.next();
+                if(input.equals("\n"))
+                    System.out.println("\"Newline character\"");
+                else
+                    if(input.equals("\r"))
+                        System.out.println("\"Carriage return character\"");
+                    else
+                        if(input.equals("\t"))
+                            System.out.println("\"Tab character\"");
+                        else
+                            System.out.println("\"" + input + "\"");
+            }
             scanner.close();
         } catch(FileNotFoundException e) {
             e.printStackTrace();
             System.err.println("This is an unexpected exception; the config file should exist when the ConfigReader is created.");
             System.exit(1);
         }
-    }
-
-    public static void main(String[] args) {
-        ConfigReader reader = new ConfigReader(new File("jOmetry.config"));
-        reader.read();
-        System.out.println(reader.fileLines);
     }
 
 }
