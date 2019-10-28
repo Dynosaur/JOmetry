011abb4bffabe0a59181a67ab94d12102273cd91
Dynosaur <heracassia@gmail.com>
2019-10-27 21:30:27 -0500

    Uses ConfigReader

--- src/main/java/rsrc/ResourceManager.java
+++ src/main/java/rsrc/ResourceManager.java
@@ -1,9 +1,9 @@
 package rsrc;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * The Resource Manager will control configurations for the program. This includes reading and writing to configuration
@@ -11,10 +11,11 @@ import java.io.PrintWriter;
  *
  * @author  Alejandro Doberenz
  * @since   10/17/2019
- * @version 0.2
+ * @version 0.25
  */
 public class ResourceManager {
 
+    // <editor-fold desc="Variables">
     private static boolean isFirstTime = true;
 
     private static final String MAIN_LOGS_PATH = "src\\main\\resources\\logs";
@@ -23,61 +24,91 @@ public class ResourceManager {
     private static File configFile = new File("jOmetry.config");
     private static File mainLogDirectory = new File(MAIN_LOGS_PATH);
     private static File testLogDirectory = new File(TEST_LOGS_PATH);
+    // </editor-fold>
 
     public static void start() {
-        isFirstTime = checkIsFirstTime();
-        if(isFirstTime) {
-            System.out.println("Running first time setup...");
-            try {
-                if(configFile.createNewFile()) System.out.println("Created jOmetry/jOmetry.config");
-                if(mainLogDirectory.mkdirs()) System.out.println("Created src/main/resources/logs");
-                if(testLogDirectory.mkdirs()) System.out.println("Created src/test/resources/logs");
-            } catch(IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-            System.out.println("Writing default configurations...");
-            try {
-                PrintWriter out = new PrintWriter(configFile);
-                out.println("# Alphabet");
-                out.println("    ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n    abcdefghijklmnopqrstuvwxyz");
-                out.println("# Legal Text");
-                out.println("    $Alphabet");
-                out.println("    +-*/=[]{}()");
-                out.close();
-            } catch(IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-            System.out.println("Done.");
+        isFirstTime = !(
+                configFile.exists()
+                && mainLogDirectory.exists()
+                && testLogDirectory.exists()
+        );
+
+        if(isFirstTime)
+            createConfigurations(true);
+
+        System.out.println("Loading configurations...");
+        ConfigReader reader = new ConfigReader(configFile);
+        reader.read();
+    }
+
+    public static void createConfigurations(boolean verbose) {
+        if(verbose) System.out.println("Running first time setup...");
+        try {
+            if(configFile.createNewFile())
+                System.out.println("Created jOmetry/jOmetry.config");
+            if(mainLogDirectory.mkdirs())
+                System.out.println("Created src/main/resources/logs");
+            if(testLogDirectory.mkdirs())
+                System.out.println("Created src/test/resources/logs");
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);
         }
 
+        System.out.println("Writing default configurations...");
+        try {
+            PrintWriter out = new PrintWriter(configFile);
+            out.println("# Alphabet");
+            out.println("\tABCDEFGHIJKLMNOPQRSTUVWXYZ\n\tabcdefghijklmnopqrstuvwxyz");
+            out.println("# Legal Text");
+            out.println("\t$Alphabet");
+            out.println("\t+-*/=[]{}()");
+            out.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("Done.\n");
     }
 
-    private static boolean checkIsFirstTime() {
-        return !(configFile.exists() && mainLogDirectory.exists() && testLogDirectory.exists());
+    public static void purgeConfigurations(boolean verbose) {
+        if(verbose) {
+            System.out.println("\n\t=======================");
+            System.out.println("\t\t\tPURGING ");
+            System.out.println("Purging configuration and log files...");
+        }
+        printDelete(configFile, verbose);
+        printDelete(mainLogDirectory, verbose);
+        printDelete(testLogDirectory, verbose);
+        if(verbose) {
+            System.out.println("Done.");
+            System.out.println("\t=======================");
+        }
     }
 
-    public static void purgeConfigurations() {
-        System.out.println("\n\t=======================");
-        System.out.println("\t\t\tPURGING ");
-        System.out.println("Purging configuration and log files...");
-        printDelete(configFile);
-        printDelete(mainLogDirectory);
-        printDelete(testLogDirectory);
-        System.out.println("Done.");
-        System.out.println("\n\t=======================");
-    }
-
-    private static void printDelete(File fileToDelete) {
-        System.out.print(fileToDelete.getPath() + " deleted: [ ");
-        if(fileToDelete.isDirectory())
-            if(recursiveDelete(fileToDelete)) System.out.print("x");
-            else System.out.print(" ");
-        else
-            if(fileToDelete.delete()) System.out.print("x");
-            else System.out.print(" ");
-        System.out.println(" ]");
+    private static void printDelete(File fileToDelete, boolean verbose) {
+        if(verbose)
+            System.out.print(fileToDelete.getPath() + " deleted: [ ");
+        if(fileToDelete.isDirectory()) {
+            if(verbose) {
+                if(recursiveDelete(fileToDelete))
+                    System.out.print("x");
+                else
+                    System.out.print(" ");
+            } else
+                recursiveDelete(fileToDelete);
+        } else {
+            if(verbose) {
+                if(fileToDelete.delete())
+                    System.out.print("x");
+                else
+                    System.out.print(" ");
+            } else
+                fileToDelete.delete();
+        }
+        if(verbose)
+            System.out.println(" ]");
     }
 
     private static boolean recursiveDelete(File fileToDelete) {
@@ -88,9 +119,25 @@ public class ResourceManager {
         return fileToDelete.delete();
     }
 
+    public static void clean() {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        while(true) {
+            System.out.print("Are you sure you want to erase all test logs and configurations? ");
+            String input = in.nextLine().toLowerCase();
+            if(input.equals("yes") || input.equals("y")) {
+                purgeConfigurations(true);
+                return;
+            } else if(input.equals("no") || input.equals("n")) {
+                System.out.println("Purge aborted.");
+                return;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         start();
-        purgeConfigurations();
+        clean();
     }
 
 }
